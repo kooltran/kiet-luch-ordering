73b727e2e29d364563d4766c031cb3362f226cb9
kiettran <kiet.tran@s3corp.com.vn>
2020-08-28 16:16:35 +0700

    initial project

---
+++ build/precache-manifest.70a7d605918981c96eb8809de6473539.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02bd0d9b8603a2cb6ffd225871511e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c0307bfe180a484c664",
+    "url": "/static/css/2.9a566409.chunk.css"
+  },
+  {
+    "revision": "57612575ac39aab713c6",
+    "url": "/static/css/main.1735740d.chunk.css"
+  },
+  {
+    "revision": "0c0307bfe180a484c664",
+    "url": "/static/js/2.39469185.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.39469185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57612575ac39aab713c6",
+    "url": "/static/js/main.220e0606.chunk.js"
+  },
+  {
+    "revision": "213dab83e684b72c2bfe",
+    "url": "/static/js/runtime-main.c552d4ad.js"
+  },
+  {
+    "revision": "890dce6082692bf7707393840ef73b85",
+    "url": "/static/media/cart.890dce60.svg"
+  },
+  {
+    "revision": "441d8611106ab6473ed8c5487cda63ac",
+    "url": "/static/media/cross.441d8611.svg"
+  },
+  {
+    "revision": "6b4b70dc5aad5de949394d10e5e15afa",
+    "url": "/static/media/fobiden-order.6b4b70dc.svg"
+  },
+  {
+    "revision": "d41570f2e12a572e8e46cac2f7c07260",
+    "url": "/static/media/loading.d41570f2.svg"
+  },
+  {
+    "revision": "311313cded0dc23bba479b786ac0aad8",
+    "url": "/static/media/plus.311313cd.svg"
+  },
+  {
+    "revision": "db93b3b8239bfa0f8f8fc0ef0d2d25eb",
+    "url": "/static/media/tick.db93b3b8.svg"
+  }
+]);
