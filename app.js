110f3a6905c716885a4406e57bf1b9ae7d815885
kooltran <kiettrankm11@gmail.com>
2020-08-30 20:58:55 +0700

    fix add absolute path for build folder

--- app.js
+++ app.js
@@ -1,136 +1,136 @@
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-const express = require('express')
-const puppeteer = require('puppeteer')
-const bodyParser = require('body-parser')
-const passport = require('passport')
-const path = require('path')
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const express = require("express");
+const puppeteer = require("puppeteer");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
 
-const cookiesSession = require('cookie-session')
+const cookiesSession = require("cookie-session");
 
 // const cors = require('cors')
 
-const config = require('./config.json')
+const config = require("./config.json");
 
-const MenuList = require('./models/menu')
-const orderRoute = require('./routes/orderDish')
+const MenuList = require("./models/menu");
+const orderRoute = require("./routes/orderDish");
 
-require('./passport-setup')
+require("./passport-setup");
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 3000
-const db = mongoose.connection
+const app = express();
+const PORT = process.env.PORT || 3000;
+const db = mongoose.connection;
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./build", "./build/index.html"));
+});
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // app.use(cors({ credentials: true, origin: 'http://localhost:3001/' }))
 
-const URL = 'https://www.anzi.com.vn/'
+const URL = "https://www.anzi.com.vn/";
 
 mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    useCreateIndex: true
   })
-  .then(() => console.log('DB connected'))
-db.on('error', err => {
-  console.log('DB connection error:', err.message)
-})
+  .then(() => console.log("DB connected"));
+db.on("error", err => {
+  console.log("DB connection error:", err.message);
+});
 
 const getMenuList = async () => {
   try {
     const brower = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    })
-    const page = await brower.newPage()
-    await page.goto(URL)
-    const existList = await MenuList.find()
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+    const page = await brower.newPage();
+    await page.goto(URL);
+    const existList = await MenuList.find();
 
     const menuList = await page.evaluate(() => {
-      const items = document.querySelectorAll('#list_menu .item-menu')
-      let menuInfo = []
+      const items = document.querySelectorAll("#list_menu .item-menu");
+      let menuInfo = [];
       items.forEach((item = {}) => {
-        console.log(item)
-        const img = item.children[0].children[0].src
-        const name = item.children[1].textContent.trim()
-        const price = item.children[2].children[0].textContent.trim()
-        menuInfo.push({ name, img, price })
-      })
-      return menuInfo
-    })
+        console.log(item);
+        const img = item.children[0].children[0].src;
+        const name = item.children[1].textContent.trim();
+        const price = item.children[2].children[0].textContent.trim();
+        menuInfo.push({ name, img, price });
+      });
+      return menuInfo;
+    });
 
     if (existList.length === 0) {
-      MenuList.insertMany(menuList)
+      MenuList.insertMany(menuList);
     } else {
       const newList = menuList.map((item, idx) => ({
         id: existList[idx]._id,
         name: item.name,
         img: item.img,
-        price: item.price,
-      }))
+        price: item.price
+      }));
       newList.map(async (item = {}) =>
         MenuList.updateOne({ _id: item.id }, item, { upsert: false })
-      )
+      );
     }
-    return menuList
+    return menuList;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-getMenuList()
+getMenuList();
 
-app.get('/menuList', async (request, response) => {
+app.get("/menuList", async (request, response) => {
   try {
-    const res = await MenuList.find()
-    response.send(res)
+    const res = await MenuList.find();
+    response.send(res);
   } catch (error) {
-    response.status(500).send(error)
+    response.status(500).send(error);
   }
-})
+});
 
 app.use(
   cookiesSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [config.session],
-    secret: config.secret,
+    secret: config.secret
   })
-)
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get(
-  '/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
   })
-)
+);
 
-app.get('/google/callback', passport.authenticate('google'), (req, res) => {
-  res.redirect('http://localhost:3001')
-})
+app.get("/google/callback", passport.authenticate("google"), (req, res) => {
+  res.redirect("http://localhost:3001");
+});
 
-app.get('/user', (req, res) => {
+app.get("/user", (req, res) => {
   if (req.user) {
-    res.send(req.user)
+    res.send(req.user);
   } else {
-    res.send({})
+    res.send({});
   }
-})
+});
 
-app.use('/orders', orderRoute)
+app.use("/orders", orderRoute);
 
 app.listen(PORT, () => {
-  console.log('Server started on http://localhost:' + PORT)
-})
+  console.log("Server started on http://localhost:" + PORT);
+});
 
-module.exports = app
+module.exports = app;
